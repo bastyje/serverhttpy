9b800ada16f30df2f3ba7fd6fab73568f7a20eab
Sebastian Górka <sebastiangorka442@gmail.com>
2023-05-19 00:06:29 +0200

    add package description

--- setup.py
+++ setup.py
@@ -5,5 +5,8 @@ setuptools.setup(
     version='0.0.1',
     author='Sebastian Górka, Michał Ziober',
     url='https://github.com/bastyje/py-http',
-    python_requires='>=3.10'
+    python_requires='>=3.10',
+    description='Simple web server framework for Python',
+    long_description='Simple web server framework for Python',
+    long_description_content_type='text/markdown'
 )
