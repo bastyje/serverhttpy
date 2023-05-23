9343a6fad1aad32a9fef0e57ed1a35cf2098e832
Sebastian Górka <72526338+bastyje@users.noreply.github.com>
2023-05-23 23:38:17 +0200

    Update setup.py (#3)

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
 
 setuptools.setup(
-    name="serverhttpy",
-    version="0.0.1",
-    author="Sebastian Górka, Michał Ziober",
-    url="https://github.com/bastyje/py-http",
-    python_requires=">=3.10",
-    description="Simple web server framework for Python",
-    long_description="Simple web server framework for Python",
-    long_description_content_type="text/markdown",
+    name='serverhttpy',
+    version='0.0.1',
+    author='Sebastian Górka, Michał Ziober',
+    url='https://github.com/bastyje/py-http',
+    python_requires='>=3.10',
+    description='Simple web server framework for Python',
+    long_description='Simple web server framework for Python',
+    long_description_content_type='text/markdown',
 )
