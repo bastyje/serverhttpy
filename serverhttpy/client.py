6a6f0ce8d3ee5530fc8a60286164317b9c710850
Michał Ziober <michalziober08@gmail.com>
2023-05-24 00:09:32 +0200

    renamed endpoint in client

--- serverhttpy/client.py
+++ serverhttpy/client.py
@@ -5,7 +5,7 @@ import multiprocessing as mp
 processes = list()
 for i in range(10):
     processes.append(
-        mp.Process(target=lambda: requests.post("http://127.0.0.1:8080/movie"))
+        mp.Process(target=lambda: requests.post("http://127.0.0.1:8080/"))
     )
     processes[i].start()
 
