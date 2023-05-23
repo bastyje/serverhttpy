c9952864cc9780d6e14fb91598e0d90687096385
Michał Ziober <michalziober08@gmail.com>
2023-05-23 22:23:08 +0200

    moved and renamed main.py -> example_clients_managment.py

--- serverhttpy/examples/example_clients_managment.py
+++ serverhttpy/examples/example_clients_managment.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     def index(request: Request) -> Response:
         time.sleep(1.0)
         return Response(
-            StatusCode.OK, body="data", headers={"Content-Type": "text/plain"}
+            StatusCode.OK, body="Welcome!", headers={"Content-Type": "text/plain"}
         )
 
     def say_name_and_age(request: Request) -> Response:
@@ -100,7 +100,7 @@ if __name__ == "__main__":
         )
 
     server = Server("0.0.0.0", 8080, threads_number=10)
-    server.register_endpoint(Endpoint(Path("/movie"), HTTPMethod.POST, index))
+    server.register_endpoint(Endpoint(Path("/"), HTTPMethod.GET, index))
     server.register_endpoint(
         Endpoint(Path("/{name}/{age}"), HTTPMethod.GET, say_name_and_age)
     )
