9c67d4d27c47ef560e9eb0ba489f225ce3c30596
Michał Ziober <73911673+micha5555@users.noreply.github.com>
2023-05-24 00:02:11 +0200

    Ziober 2 (#6)
    * retrieving arguments from path
    
    * in progress
    
    * rework on getting arguments; added sample endpoints
    
    * formatted code
    
    * moved and renamed main.py -> example_clients_managment.py
    
    * changed methods to private; moved sample_data
    
    * moved examples; thread_num as console argument
    
    * changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='serverhttpy',
-    version='0.0.2',
+    version='0.0.3',
     author='Sebastian Górka, Michał Ziober',
     url='https://github.com/bastyje/serverhttpy',
     python_requires='>=3.10',
