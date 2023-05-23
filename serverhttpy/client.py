8488de3e98231be06ae73b22b01c2c32c0e8eb4b
Michał Ziober <73911673+micha5555@users.noreply.github.com>
2023-05-24 00:32:13 +0200

    Ziober 2 (#7)
    * retrieving arguments from path
    
    * in progress
    
    * rework on getting arguments; added sample endpoints
    
    * formatted code
    
    * moved and renamed main.py -> example_clients_managment.py
    
    * changed methods to private; moved sample_data
    
    * moved examples; thread_num as console argument
    
    * changed version
    
    * renamed endpoint in client
    
    * added printing time elapsed in client
    
    * changed version

--- serverhttpy/client.py
+++ serverhttpy/client.py
@@ -1,13 +1,17 @@
 import requests
 import multiprocessing as mp
-
+import time
 
 processes = list()
+start = time.time()
 for i in range(10):
     processes.append(
-        mp.Process(target=lambda: requests.post("http://127.0.0.1:8080/movie"))
+        mp.Process(target=lambda: requests.post("http://127.0.0.1:8080/"))
     )
     processes[i].start()
 
 for i in range(10):
     processes[i].join()
+
+end = time.time()
+print(f"elapsed time: {end - start}s\n")
